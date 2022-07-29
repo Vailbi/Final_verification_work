83249adc53a69a43f500eff8d6d02c995068f6b9
Fedor <vailbi94@gmail.com>
2022-07-29 23:44:10 +0300

    find newSize by newArray

--- Program.cs
+++ Program.cs
@@ -1,1 +1,9 @@
 ﻿string [] inputArray = {"one" , "two0" ,  "=_=" , "rus", "test", "00"};
+int newSize = 0;
+for (int i = 0; i < inputArray.Length; i++)
+{
+   if (inputArray[i].Length <= 3)
+   {
+    newSize++;
+   } 
+}
